ab9ade769ea1367a880db020bfe9254e1cee2a97
yamosan <yamorimasato@gmail.com>
2022-05-14 11:54:09 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 八森聖人 on 2022/05/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 八森聖人 on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
